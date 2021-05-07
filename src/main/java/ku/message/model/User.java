cd76d76171bbb51a083e4fd36dc8a143d13cd363
patdpat <s.kietchai@outlook.com>
2021-05-07 21:13:47 +0700

    secure lab finalized

---
+++ src/main/java/ku/message/model/User.java
@@ -0,0 +1,26 @@
+package ku.message.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private Instant createdAt;
+}
